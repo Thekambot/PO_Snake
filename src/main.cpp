ad28718cf4dbd07970541e5daa424611a8517e60
Thekambot <thekambot09@gmail.com>
2021-01-18 15:16:47 +0100

    Testuje se

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "winsys.h"
 #include "snake.h"
 
+#include <iostream>
+
 int main ()
 {
+  std::cout << "test" << std::endl;
+
   CDesktop d;
   d.insert(new CInputLine (CRect (CPoint (5, 7), CPoint (15, 15))));
   d.insert(new CWindow (CRect (CPoint (2, 3), CPoint (20, 10)), '#'));
